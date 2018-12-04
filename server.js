cef74623c40a90757e11f0f75b877a6471fd9aa1
glepur <goran.lepur1@gmail.com>
2018-12-04 14:58:11 +0100

    disallow modification

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const webdav = require('webdav-server').v2;
 const fs = require('fs');
 
+// class Priv extends webdav.PrivilegeManager {
+//   constructor() {
+//     super(arguments);
+//   }
+//   _can(path, user, resource, privilege, cb) {
+//     console.log(privilege);
+
+//     if (privilege.includes('Read')) {
+//       return cb(null, true);
+//     }
+//     return cb(null, false);
+//   }
+// }
+
 const server = new webdav.WebDAVServer({
   port: 3001
+  // privilegeManager: new Priv()
+});
+
+server.beforeRequest((arg, next) => {
+  console.log(arg.request.method);
+
+  if (!['PROPFIND', 'HEAD', 'GET'].includes(arg.request.method)) {
+    return arg.response.end();
+  }
+  next();
 });
 
 const rootFileSystem = server.rootFileSystem();
@@ -21,22 +45,22 @@ setTimeout(() => {
     'img2.jpg': fs.readFileSync('./server_sync_folder/img2.jpg'),
     'img3.jpg': fs.readFileSync('./server_sync_folder/img2.jpg')
   });
-}, 5000);
+}, 10000);
 
-server.afterRequest((arg, next) => {
-  // Display the method, the URI, the returned status code and the returned message
-  console.log(
-    '>>',
-    arg.request.method,
-    arg.requested.uri,
-    '>',
-    arg.response.statusCode,
-    arg.response.statusMessage
-  );
-  // If available, display the body of the response
-  console.log(arg.responseBody);
-  next();
-});
+// server.afterRequest((arg, next) => {
+//   // Display the method, the URI, the returned status code and the returned message
+//   console.log(
+//     '>>',
+//     arg.request.method,
+//     arg.requested.uri,
+//     '>',
+//     arg.response.statusCode,
+//     arg.response.statusMessage
+//   );
+//   // If available, display the body of the response
+//   console.log(arg.responseBody);
+//   next();
+// });
 
 server.start(httpServer => {
   console.log(
